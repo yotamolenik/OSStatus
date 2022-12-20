7763b444c9ab96c5086ccb390533d5cdc6318ea6
Yotam Olenik <yotamolenik@gmail.com>
2022-12-20 18:23:05 +0200

    osstatus: add print_all and get_all options

--- osstatus/__main__.py
+++ osstatus/__main__.py
@@ -3,19 +3,35 @@ from bs4 import BeautifulSoup
 import click
 
 
+
+def cli(ctx, directory):
+    """ Parse .mobileconfig files in a given directory """
+    ctx.ensure_object(dict)
+    ctx.obj['directory'] = directory
+
+
 @click.group()
-def cli():
-    pass
+@click.pass_context
+def cli(ctx):
+    ctx.ensure_object(dict)
+    response = requests.get("https://www.osstatus.com/search/results?platform=all&framework=all&search=")
+    soup = BeautifulSoup(response.text, 'html.parser')
+    tbody = soup.find('tbody')
+    trs = tbody.find_all('tr')
+    ctx.obj['table_rows'] = trs
 
 
 @click.command()
-def print_all():
-    pass
+@click.pass_context
+def print_all(ctx):
+    for tr in ctx.obj['table_rows']:
+        print(tr)
 
 
 @click.command()
-def get_all():
-    pass
+@click.pass_context
+def get_all(ctx):
+    return ctx.obj['table_rows']
 
 
 if __name__ == '__main__':
