7b7b29d229c0c86a62ee6ba66dd6fc6f77fb7854
Yotam Olenik <yotamolenik@gmail.com>
2022-12-20 18:59:52 +0200

    cli: add get_code_info command

--- osstatus/__main__.py
+++ osstatus/__main__.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 def cli(ctx):
     """ get error code information from the OSStatus website """
     ctx.ensure_object(dict)
-    response = requests.get("https://www.osstatus.com/search/results?platform=all&framework=all&search=")
+    response = requests.get('https://www.osstatus.com/search/results?platform=all&framework=all&search=')
     soup = BeautifulSoup(response.text, 'html.parser')
     tbody = soup.find('tbody')
     trs = tbody.find_all('tr')
@@ -28,6 +28,18 @@ def print_all(ctx):
 def get_all(ctx):
     """ return all errors """
     return ctx.obj['table_rows']
+
+
+@cli.command()
+@click.argument('code', type=int)
+def get_code_info(code):
+    """ get information about a single error code """
+    response = requests.get(f'https://www.osstatus.com/search/results?platform=all&framework=all&search={code}')
+    soup = BeautifulSoup(response.text, 'html.parser')
+    tbody = soup.find('tbody')
+    trs = tbody.find_all('tr')
+    for tr in trs:
+        print(tr)
 
 
 if __name__ == '__main__':
