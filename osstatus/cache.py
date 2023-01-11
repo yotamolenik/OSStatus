efe0e16a800b94eff752ad4bdcdb6ee63e1fc57d
doron zarhi <doron88@gmail.com>
2023-01-11 09:41:46 +0200

    use cache.pickle for querying

---
+++ osstatus/cache.py
@@ -0,0 +1,32 @@
+import dataclasses
+import pickle
+from enum import Enum
+from functools import cache
+from pathlib import Path
+from typing import List, Mapping, Optional
+
+CACHE_FILE = Path(__file__).parent / 'cache.pickle'
+
+
+class Platform(Enum):
+    macOS = 'macOS'
+    iOS = 'iOS'
+
+
+@dataclasses.dataclass
+class ErrorCode:
+    platforms: List[Platform]
+    framework: str
+    header_file: str
+    name: str
+    value: int
+    description: str
+
+
+@cache
+def get_all_errors_codes() -> Mapping[int, List[ErrorCode]]:
+    return pickle.loads(CACHE_FILE.read_bytes())
+
+
+def get_possible_error_codes(value: int) -> Optional[List[ErrorCode]]:
+    return get_all_errors_codes().get(value)
